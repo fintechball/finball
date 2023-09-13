b46af706d3c0fe1969896f09136e68f8b3afd8fc
seongho <tjdghgk98@gmail.com>
2023-09-13 16:47:46 +0900

    Issue : #1 company Entity 수정

--- backend/mydata/src/main/java/com/finball/mydata/entity/Company.java
+++ backend/mydata/src/main/java/com/finball/mydata/entity/Company.java
@@ -1,17 +1,31 @@
 package com.finball.mydata.entity;
 
+import com.finball.mydata.type.CompanyType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Company {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String cpName;
 
     private String cpLogo;
+
+    private Long cpCode;
+
+    private CompanyType cpType;
 }
