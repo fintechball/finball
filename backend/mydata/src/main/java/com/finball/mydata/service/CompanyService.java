c32c8a6490cb721d6ed94aaae78e179df4a41e7e
jybin96 <wjddudqls96@gmail.com>
2023-09-14 15:05:47 +0900

    Feat : 은행리스트 불러오기 API 추가
    - cpType이 은행사인 데이터들을 불러오는 기능을 추가했습니다.

--- backend/mydata/src/main/java/com/finball/mydata/service/CompanyService.java
+++ backend/mydata/src/main/java/com/finball/mydata/service/CompanyService.java
@@ -1,6 +1,13 @@
 package com.finball.mydata.service;
 
+import com.finball.mydata.dto.company.BankInfoDto;
+import com.finball.mydata.dto.company.GetBankListDto;
+import com.finball.mydata.dto.company.GetBankListDto.Response;
+import com.finball.mydata.entity.Company;
 import com.finball.mydata.repository.CompanyRepository;
+import com.finball.mydata.type.CompanyType;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +15,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class CompanyService {
     private final CompanyRepository companyRepository;
+
+    public GetBankListDto.Response getBanks() {
+        System.out.println("asdasdasdasdasdasdas");
+        List<Company> companies = companyRepository.findAllByCpType(CompanyType.은행사);
+        List<BankInfoDto> bankInfoDtoList = new ArrayList<>();
+        System.out.println(bankInfoDtoList);
+
+        for(Company company : companies){
+            bankInfoDtoList.add(company.toBankInfoDto());
+        }
+
+        System.out.println(bankInfoDtoList);
+
+        return GetBankListDto.Response.builder()
+                .bankList(bankInfoDtoList)
+                .build();
+    }
 }
