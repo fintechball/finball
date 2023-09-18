791c32646cdcaab985e879502897f3e13aa026cb
seongho <tjdghgk98@gmail.com>
2023-09-18 10:22:44 +0900

    Feat : 에러 핸들러 수정 - custom 에러 => ErrorCode에 추가만 후 사용하면 됨 - custom 에러에 없는 에러 => INTERNAL_SERVER_ERROR발생

--- backend/backend/src/main/java/com/example/backend/error/ErrorCode.java
+++ backend/backend/src/main/java/com/example/backend/error/ErrorCode.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    BAD_REQUEST_ERROR1(400, "1번으로 요청했는데, 에러입니다."),
-    ALREADY_IN_USE(409, "이미 데이터가 존재합니다.");
+    ALREADY_IN_USE(409, "이미 데이터가 존재합니다."),
+    INTERNAL_SERVER_ERROR(500, "서버 에러입니다");
 
 
     private final int status;
--- backend/backend/src/main/java/com/example/backend/error/ResponseUtil.java
+++ backend/backend/src/main/java/com/example/backend/error/ResponseUtil.java
@@ -5,9 +5,9 @@ import org.springframework.http.ResponseEntity;
 
 public class ResponseUtil {
 
-    public static ResponseEntity<ErrorResponse> createErrorResponse(ErrorCode code) {
-        return new ResponseEntity<>(ErrorResponse.of(code), HttpStatus.valueOf(code.getStatus()));
-    }
+//    public static ResponseEntity<ErrorResponse> createErrorResponse(ErrorCode code) {
+//        return new ResponseEntity<>(ErrorResponse.of(code), HttpStatus.valueOf(code.getStatus()));
+//    }
 
 //  public static ResponseEntity<ErrorResponse> createSuccessResponse(ErrorCode code){
 //    return new ResponseEntity<>(ErrorResponse.of(code), HttpStatus.valueOf(code.getStatus()));
