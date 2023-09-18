b9bdeda91173418a00f6ef4a52d480a6fcede660
unknown <bluemin_@naver.com>
2023-09-18 16:41:21 +0900

    Feat : module.scss 적용 module.scss 적용

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.scss";
+
+function Header() {
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1>Header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
