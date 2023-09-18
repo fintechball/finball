f53c0738852c33f55b0230743f0cd14c36b79e37
unknown <bluemin_@naver.com>
2023-09-18 11:47:30 +0900

    Feat : Prettier적용, redux api 연결 Prettier적용, redux api 연결

--- frontend/src/store/rootReducer.ts
+++ frontend/src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import counter from "./slices/counterSlice";
+import test from "./slices/testSlice";
 
 const reducer = combineReducers({
   counter,
+  test,
 });
 
 export type ReducerType = ReturnType<typeof reducer>;
