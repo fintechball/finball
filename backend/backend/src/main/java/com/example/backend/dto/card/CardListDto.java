29c5f0ee19fa26d2677f7ab09872248526f7df45
김정희 <spor1998@naver.com>
2023-09-18 15:29:16 +0900

    Feat : 등록할 사용자 카드 조회 API 추가

---
+++ backend/backend/src/main/java/com/example/backend/dto/card/CardListDto.java
@@ -0,0 +1,21 @@
+package com.example.backend.dto.card;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class CardListDto {
+
+    @Data
+    public static class Request {
+        private List<Long> cardCompanyCodeList;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Response {
+
+        private List<CardInfo> cardInfoList;
+    }
+
+}
