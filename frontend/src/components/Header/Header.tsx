b344b135b33b5832f1eec9b4b8a5af4c0c8ce830
unknown <bluemin_@naver.com>
2023-09-18 17:33:28 +0900

    Feat : Header 임시 추가 Header 임시 추가

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from "./Header.module.scss";
+import finballLogo from "../../assets/finballLogo.png";
 
 function Header() {
+  const title = "";
+
   return (
     <div>
       <div className={styles.header}>
-        <h1>Header</h1>
+        <img src={finballLogo} alt="Finball Logo" />
+        <p>{title}</p>
       </div>
     </div>
   );
