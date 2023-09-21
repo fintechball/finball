d5e6babcba887f9e76070d01ee4e3aa402ad9109
unknown <bluemin_@naver.com>
2023-09-21 16:21:34 +0900

    Feat : 회원가입 완료 회원가입 완료

--- frontend/src/pages/Auth/SignUpCerti.tsx
+++ frontend/src/pages/Auth/SignUpCerti.tsx
@@ -1,7 +1,5 @@
 import { useLocation } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { useState } from "react";
 
 interface FormData {
   name: string;
@@ -10,66 +8,111 @@ interface FormData {
   phoneNumber: string;
 }
 
-const schema = yup.object({
-  phoneNumber: yup
-    .string()
-    .required("휴대폰 번호는 필수 입력 항목입니다.")
-    .matches(/^\d{11}$/, "정확한 휴대폰 번호를 입력해 주세요."),
-});
-
 function SignUpCerti() {
   const location = useLocation();
   const formData = location.state?.formData;
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({ mode: "onChange", resolver: yupResolver(schema) });
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [isPhoneValid, setIsPhoneValid] = useState(false);
+  const [validCode, setValidCode] = useState("xxxx");
+  const [code, setCode] = useState("");
 
-  const getCode = () => {};
-
-  const onSubmit = async (data: FormData) => {
-    console.log(data);
-    const updatedFormData: FormData = {
-      ...formData,
-      phoneNumber: data.phoneNumber,
-    };
+  const getCode = async () => {
     try {
-      const requestBody = JSON.stringify(updatedFormData);
+      const requestBody = JSON.stringify({
+        recipientPhoneNumber: phoneNumber,
+      });
 
-      const response = await fetch(`https://j9e106.p.ssafy.io/user`, {
+      const response = await fetch(`https://j9e106.p.ssafy.io/user/sms`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: requestBody,
       });
-
+      console.log(requestBody);
       console.log(response);
       if (response.status === 200) {
         const responseData = await response.json();
-        alert(responseData.message);
+        alert("sms 인증 요청을 보냈습니다. 인증 번호를 입력해주세요.");
+        setValidCode(responseData.data.certificationNumber);
+        console.log("인증번호:", responseData.data.certificationNumber);
+      } else {
+        alert("죄송합니다. 잠시 후 다시 시도해주세요.");
       }
     } catch (error) {
-      console.error("데이터 전송 실패", error);
+      console.error(error);
     }
+  };
 
-    console.log(updatedFormData);
+  const codeCheck = () => {
+    if (code === validCode) {
+      alert("인증이 완료되었습니다.");
+      setIsPhoneValid(true);
+    }
+  };
+
+  const onSubmit = async () => {
+    console.log(isPhoneValid);
+    if (isPhoneValid) {
+      const updatedFormData: FormData = {
+        ...formData,
+        phoneNumber: phoneNumber,
+      };
+      try {
+        const requestBody = JSON.stringify(updatedFormData);
+
+        const response = await fetch(`https://j9e106.p.ssafy.io/user`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: requestBody,
+        });
+
+        console.log(response);
+        if (response.status === 200) {
+          const responseData = await response.json();
+          alert(responseData.message);
+        }
+      } catch (error) {
+        console.error("데이터 전송 실패", error);
+      }
+
+      console.log(updatedFormData);
+    } else {
+      alert("휴대폰 인증을 완료해주세요.");
+    }
   };
 
   return (
     <div>
       <h2>휴대폰 인증</h2>
       <p>휴대폰 번호</p>
-      <input placeholder="Phone" {...register("phoneNumber")} />
-      {errors.phoneNumber && <p>{errors.phoneNumber.message}</p>}
+      <input
+        type="text"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+        disabled={isPhoneValid}
+      />
 
       <button type="button" onClick={getCode}>
         인증번호 전송
       </button>
 
-      <button type="button" onClick={handleSubmit(onSubmit)}>
+      <p>인증번호 입력</p>
+      <input
+        type="text"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        disabled={isPhoneValid}
+      />
+
+      <button type="button" onClick={codeCheck}>
+        인증하기
+      </button>
+
+      <button type="button" onClick={onSubmit}>
         가입하기
       </button>
     </div>
