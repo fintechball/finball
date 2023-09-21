a92bc6e039a2609324fc606d489df222cc547cc0
김정희 <spor1998@naver.com>
2023-09-21 12:51:47 +0900

    Feat : sms 기능 추가

---
+++ backend/backend/src/main/java/com/example/backend/controller/SmsController.java
@@ -0,0 +1,29 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.Response;
+import com.example.backend.dto.sms.RequestDto;
+import com.example.backend.dto.sms.SmsResponse;
+import com.example.backend.service.SmsService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequiredArgsConstructor
+public class SmsController {
+
+    private final SmsService smsService;
+
+    @PostMapping("/user/sms")
+    public Response<SmsResponse> test(@RequestBody RequestDto request) throws NoSuchAlgorithmException, URISyntaxException, UnsupportedEncodingException, InvalidKeyException, JsonProcessingException {
+        SmsResponse data = smsService.sendSms(request.getRecipientPhoneNumber());
+        return new Response(200, "sms 요청 성공", data);
+    }
+}
