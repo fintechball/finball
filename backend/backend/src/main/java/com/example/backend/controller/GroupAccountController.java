b55c0ec850bf97ed71d350692b1cbb78732c42d9
seongho <tjdghgk98@gmail.com>
2023-09-22 16:54:39 +0900

    Feat : health check url에 api를 뺌

--- backend/backend/src/main/java/com/example/backend/controller/GroupAccountController.java
+++ backend/backend/src/main/java/com/example/backend/controller/GroupAccountController.java
@@ -68,6 +68,7 @@ public class GroupAccountController {
         groupAccountTransferService.transferGroupAccount(request, member);
 
         return new Response<>(200, "이체가 완료되었습니다.");
+    }
 
     @DeleteMapping("/group/account")
     public Response deleteGroupAccount(@RequestBody DeleteGroupAccountDto.Request request,
