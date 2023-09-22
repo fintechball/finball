acaf37cd29e7155cdee61999543c715d7f7cb631
김정희 <spor1998@naver.com>
2023-09-22 16:04:11 +0900

    Feat : requestMapping

--- backend/backend/src/main/java/com/example/backend/controller/CardController.java
+++ backend/backend/src/main/java/com/example/backend/controller/CardController.java
@@ -14,10 +14,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class CardController {
 
--- backend/backend/src/main/java/com/example/backend/controller/MemberController.java
+++ backend/backend/src/main/java/com/example/backend/controller/MemberController.java
@@ -11,12 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
 
