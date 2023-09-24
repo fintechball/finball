2e6abd9abc1dbd26b579bafc439300227ad668e5
spor1998 <spor1998@naver.com>
2023-09-25 00:44:40 +0900

    Fix : 사용자 계좌 목록 조회 수정

--- backend/backend/src/main/java/com/example/backend/dto/company/CompanyDto.java
+++ backend/backend/src/main/java/com/example/backend/dto/company/CompanyDto.java
@@ -1,12 +1,14 @@
 package com.example.backend.dto.company;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CompanyDto {
 
     String name;
-    String img;
+    String logo;
     Long code;
     boolean isConnected;
 
