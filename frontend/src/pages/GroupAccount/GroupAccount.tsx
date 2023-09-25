d6d2b368782a31e940d71a2a48d882e0d2b3013c
hyunwoo <wjdgusdndi@gmail.com>
2023-09-26 03:21:26 +0900

    Feat : group account front

--- frontend/src/pages/GroupAccount/GroupAccount.tsx
+++ frontend/src/pages/GroupAccount/GroupAccount.tsx
@@ -1,14 +1,39 @@
 import * as React from 'react';
+import { useState, useEffect } from "react";
 import styles from "./GroupAccount.module.css"
 import Pinball from "../Pinball/Pinball";
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const GroupAccount = () => {
- 
+  const [groupAccountData, setGroupAccountData] = useState([]);
+  const [value, setValue] = useState({cost: 0, parent: "home-canvas"});
+  const authData = useSelector(state => state.user)
+  // console.log(authData);
+  useEffect((
 
+  ) => {
+    console.log("hi");
+    axios({
+      method: "get",
+      // url: `https://j9e106.p.ssafy.io/api/group/account/31942-202934-614`,
+      url: `http://localhost:8080/api/group/account/31942-202934-614`,
+      headers: {
+        Authorization: authData.accessToken
+      },
+    }) 
+    .then((res) => {
+      setGroupAccountData(res.data.data)
+      // setValue({cost : res.data.data.balance, parent: "home-canvas"})
+      setValue({...value, cost:res.data.data.balance})
+    })
+  },[])
+  // console.log(groupAccountData);
+ 
   return (
     <div>
         <div id="home-canvas" style={{ width: "300px", height: "150px" }}>
-          <Pinball />
+          <Pinball value={value}/>
         </div>
     </div>
   );
