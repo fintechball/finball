43d6684d15f9ac0a798e2084cb4125e492134e7c
김정희 <spor1998@naver.com>
2023-09-26 14:47:43 +0900

    Feat : 은행 가져오기 API 연결 완료

--- frontend/src/components/BankAccount/BankAccount.tsx
+++ frontend/src/components/BankAccount/BankAccount.tsx
@@ -9,53 +9,22 @@ const BASE_HTTP_URL = "https://j9E106.p.ssafy.io";
 function BankAccount() {
   const navigate = useNavigate();
   const [accountList, setAccountList] = useState<any>(null);
-  const [accessToken, setAccessToken] = useState<String>("");
+  const auth = useSelector((state) => state.auth);
 
   useEffect(() => {
-    const jsonString = localStorage.getItem("persist:root");
-    if (jsonString) {
-      const jsonObject: { auth: string } = JSON.parse(jsonString);
-      const authData = JSON.parse(jsonObject.auth);
-      const accessToken = authData.accessToken;
-
-      if (accessToken) {
-        setAccessToken(accessToken);
-        getBankList(accessToken);
-      } else {
-        console.log("accessToken이 존재하지 않습니다.");
-      }
-    } else {
-      console.log("localStorage가 존재하지 않습니다.");
-    }
+    getBankList();
   }, []);
 
-  const getBankList = (accessToken) => {
-    axios
-      .get(`${BASE_HTTP_URL}/api/user/account/simple`, {
-        headers: {
-          Authorization: accessToken,
-        },
-      })
-      .then((response) => {
-        setAccountList(response.data.data.userAccountSimpleList);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const transfer = (accountId) => {
+  const getBankList = () => {
     axios
       .get(`${BASE_HTTP_URL}/api/user/account`, {
         headers: {
-          Authorization: accessToken,
+          Authorization: auth.accessToken,
         },
       })
       .then((response) => {
-        const userAccountList = response.data.data.userAccountList;
-        navigate("/accountDetail", {
-          state: userAccountList.find((item) => item.accountNo === accountId),
-        });
+        setAccountList(response.data.data.userAccountList);
+        console.log(response.data.data.userAccountList);
       })
       .catch((error) => {
         console.log(error);
@@ -67,12 +36,12 @@ function BankAccount() {
       {accountList && accountList.length !== 0 ? (
         [...accountList].map((account, index) => (
           <div className={styles.container} key={index}>
-            <img src={account.bankImage} width={50} height={50} />
+            <img src={account.company.logo} width={50} height={50} />
             <div>
-              <p>{account.name}</p>
-              <p>{account.account}</p>
+              <p>{account.account.name}</p>
+              <p>{account.account.no}</p>
             </div>
-            <button onClick={() => transfer(account.account)}>송금</button>
+            <button>송금</button>
           </div>
         ))
       ) : (
