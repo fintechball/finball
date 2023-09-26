e96f0e81d8dc8ce72b019382db0cef1f3cd2cf67
unknown <bluemin_@naver.com>
2023-09-26 16:59:09 +0900

    Fix : 계좌생성화면 CSS 수정 계좌생성화면 CSS 수정

--- frontend/src/pages/Pinball/CreateFinBallAccount.tsx
+++ frontend/src/pages/Pinball/CreateFinBallAccount.tsx
@@ -2,136 +2,162 @@ import { Button } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./CreateFinBallAccount.module.scss";
+
+import { Dropdown, Menu, Space, Checkbox } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 const BASE_HTTP_URL = "https://j9e106.p.ssafy.io";
 //const BASE_HTTP_URL = "http://localhost:8080";
 function CreateFinBallAccount() {
+  const navigate = useNavigate();
+  const [usageList, setUsageList] = useState<string[]>([]);
+  const [moneySourceList, setMoneySourceList] = useState<string[]>([]);
+  const [accessToken, setAccessToken] = useState<string>("");
+  const [usage, setUsage] = useState("");
+  const [moneySource, setMoneySource] = useState("");
+  const [isTexted, setIsTexted] = useState(false);
 
-    const navigate = useNavigate();
-    const [usageList, setUsageList] = useState<string[]>([]);
-    const [moneySourceList, setMoneySourceList] = useState<string[]>([]);
-    const [accessToken, setAccessToken] = useState<string>("");
-    const [usage, setUsage] = useState("");
-    const [moneySource, setMoneySource] = useState("");
-    const [isTexted, setIsTexted] = useState(false);
-
-    const validationCheck = () => {
-        if (usage == "") {
-            alert("사용 목적을 선택해주세요")
-            return false;
-        }
-        if (moneySource == "") {
-            alert("자금 출처를 선택해주세요")
-            return false;
-        }
-
-        return true;
+  const validationCheck = () => {
+    if (usage == "") {
+      alert("사용 목적을 선택해주세요");
+      return false;
     }
-    const createFinBallAccount = () => {
-        const headers: Record<string, string> = {
-            'Authorization': accessToken,
-            'Content-Type': 'application/json'
-        }
-
-        if (!validationCheck()) {
-            return;
-        }
-
-        axios.post(`${BASE_HTTP_URL}/api/account/fin-ball`,
-            {
-                usage: usage,
-                moneySource: moneySource,
-                isTexted: isTexted
-            },
-            {
-                headers: headers
-            })
-            .then((res) => {
-                alert("계좌 생성이 완료되었습니다.");
-                console.log("계좌 생성 완료 " + res);
-                navigate("/");
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
+    if (moneySource == "") {
+      alert("자금 출처를 선택해주세요");
+      return false;
     }
 
-    const getUsageAndMoneySource = (accessToken: string) => {
+    return true;
+  };
+  const createFinBallAccount = () => {
+    const headers: Record<string, string> = {
+      Authorization: accessToken,
+      "Content-Type": "application/json",
+    };
 
-        const headers = {
-            'Authorization': accessToken
-        }
-
-        axios.get(`${BASE_HTTP_URL}/api/usage-and-moneysource`,
-            {
-                headers: headers
-            })
-            .then((res) => {
-                console.log("list들 조회해왔습니다.")
-                setUsageList(res.data.data.usageList);
-                setMoneySourceList(res.data.data.moneySourceList);
-
-                setUsage(usageList[0])
-                setMoneySource(moneySourceList[0])
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+    if (!validationCheck()) {
+      return;
     }
 
-    useEffect(() => {
-        const jsonString = localStorage.getItem("persist:root");
+    axios
+      .post(
+        `${BASE_HTTP_URL}/api/account/fin-ball`,
+        {
+          usage: usage,
+          moneySource: moneySource,
+          isTexted: isTexted,
+        },
+        {
+          headers: headers,
+        }
+      )
+      .then((res) => {
+        alert("계좌 생성이 완료되었습니다.");
+        console.log("계좌 생성 완료 " + res);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-        const fetchData = () => {
-            if (jsonString) {
-                const jsonObject = JSON.parse(jsonString);
-                const authData = JSON.parse(jsonObject.auth);
-                const accessToken = authData.accessToken;
+  const getUsageAndMoneySource = (accessToken: string) => {
+    const headers = {
+      Authorization: accessToken,
+    };
 
-                if (accessToken) {
-                    setAccessToken(accessToken);
-                    getUsageAndMoneySource(accessToken);
-                } else {
-                    console.log("accessToken이 존재하지 않습니다.");
-                }
-            } else {
-                console.log("localStorage가 존재하지 않습니다.");
-            }
-        };
-        fetchData();
-    }, []);
+    axios
+      .get(`${BASE_HTTP_URL}/api/usage-and-moneysource`, {
+        headers: headers,
+      })
+      .then((res) => {
+        console.log("list들 조회해왔습니다.");
+        setUsageList(res.data.data.usageList);
+        setMoneySourceList(res.data.data.moneySourceList);
 
-    return (
-        <div>
-            <h1>계좌생성</h1>
-            <hr />
-            <p>안전한 거래를 위해 아래의 내용을 확인해주세요.</p>
-            <label htmlFor="usageSelectBox">
-                사용목적
-            </label>
-            <select id="usageSelectBox" name="usage" onChange={(event) => setUsage(event.target.value)}>
-                {usageList.map((usage, index) => (
-                    <option key={index} value={usage}>{usage}</option>
-                ))}
-            </select>
-            <br />
-            <label htmlFor="moneySourceSelectBox">
-                자금출처
-            </label>
-            <select id="moneySourceSelectBox" name="moneySource" onChange={(event) => setMoneySource(event.target.value)}>
-                {moneySourceList.map((moneySource, index) => (
-                    <option key={index} value={moneySource}>{moneySource}</option>
-                ))}
-            </select>
-            <br />
-            <label htmlFor="isTexted">해외에 세금을 내고 있나요?</label>
-            <input id="isTexted" type="checkbox" name="isTexted" onChange={(event) => setIsTexted(event.target.checked)}></input>
+        setUsage(usageList[0]);
+        setMoneySource(moneySourceList[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-            <Button type="primary" onClick={createFinBallAccount}>
-                계좌 생성하기
-            </Button>
-        </div>
-    )
+  useEffect(() => {
+    const jsonString = localStorage.getItem("persist:root");
+
+    const fetchData = () => {
+      if (jsonString) {
+        const jsonObject = JSON.parse(jsonString);
+        const authData = JSON.parse(jsonObject.auth);
+        const accessToken = authData.accessToken;
+
+        if (accessToken) {
+          setAccessToken(accessToken);
+          getUsageAndMoneySource(accessToken);
+        } else {
+          console.log("accessToken이 존재하지 않습니다.");
+        }
+      } else {
+        console.log("localStorage가 존재하지 않습니다.");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const usageMenu = (
+    <Menu onClick={(e) => setUsage(e.key)}>
+      {usageList.map((usage) => (
+        <Menu.Item key={usage}>{usage}</Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  const moneySourceMenu = (
+    <Menu onClick={(e) => setMoneySource(e.key)}>
+      {moneySourceList.map((moneySource) => (
+        <Menu.Item key={moneySource}>{moneySource}</Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  return (
+    <div className={styles.container}>
+      <h2>
+        안전한 거래를 위해
+        <br />
+        아래의 내용을 확인해주세요.
+      </h2>
+      <label>사용목적</label>
+      <Dropdown overlay={usageMenu}>
+        <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+          {usage || "선택하세요"} <DownOutlined />
+        </a>
+      </Dropdown>
+      <br />
+      <label>자금출처</label>
+      <Dropdown overlay={moneySourceMenu}>
+        <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+          {moneySource || "선택하세요"} <DownOutlined />
+        </a>
+      </Dropdown>
+      <br />
+      <div>
+        <span>해외에 세금을 내고 있나요?</span>
+        <Checkbox
+          id="isTexted"
+          name="isTexted"
+          onChange={(event) => setIsTexted(event.target.checked)}
+          className={styles.checkbox}
+        />
+      </div>
+      <br />
+      <Button type="primary" onClick={createFinBallAccount}>
+        계좌 생성하기
+      </Button>
+    </div>
+  );
 }
 
 export default CreateFinBallAccount;
