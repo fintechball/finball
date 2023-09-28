3525081109c32cdb36ebbacbdedb19727a35b497
spor1998 <spor1998@naver.com>
2023-09-28 22:34:00 +0900

    Fix : 줄바꿈 수정

--- frontend/src/pages/Auth/SecuritySetting.tsx
+++ frontend/src/pages/Auth/SecuritySetting.tsx
@@ -1,172 +1,178 @@
-import React, { useState, MouseEvent, useEffect,useCallback } from "react"
+import React, { useState, MouseEvent, useEffect, useCallback } from "react";
 import axios from "axios";
-import styles from "./SecurityKeypad.module.css"
-import Password from "./Certification"
+import styles from "./SecurityKeypad.module.css";
+import Password from "./Certification";
 import { RootState } from "../../store/store";
 import { useSelector } from "react-redux";
 
 const BASE_HTTP_URL = "https://j9e106.p.ssafy.io";
-const PASSWORD_MAX_LENGTH = 6 // 비밀번호 입력길이 제한 설정
-const PASSWORD_INPUT_TEXT = "설정할 비밀번호를 입력하세요."
-const PASSWORD_REINPUT_Text = "설정할 비밀번호를 다시 입력해주세요."
+const PASSWORD_MAX_LENGTH = 6; // 비밀번호 입력길이 제한 설정
+const PASSWORD_INPUT_TEXT = "설정할 비밀번호를 입력하세요.";
+const PASSWORD_REINPUT_Text = "설정할 비밀번호를 다시 입력해주세요.";
 
 const shuffle = (nums: number[]) => {
   // 배열 섞는 함수
-  let num_length = nums.length
+  let num_length = nums.length;
   while (num_length) {
-    let random_index = Math.floor(num_length-- * Math.random())
-    let temp = nums[random_index]
-    nums[random_index] = nums[num_length]
-    nums[num_length] = temp
+    let random_index = Math.floor(num_length-- * Math.random());
+    let temp = nums[random_index];
+    nums[random_index] = nums[num_length];
+    nums[num_length] = temp;
   }
-  return nums
-}
+  return nums;
+};
 
 const SecuritySetting = () => {
-  let nums_init = Array.from({ length: 10 }, (v, k) => k)
-  const auth = useSelector((state : RootState) => state.auth);
-  const [nums, setNums] = useState([...nums_init,'',' '])
+  let nums_init = Array.from({ length: 10 }, (v, k) => k);
+  const auth = useSelector((state: RootState) => state.auth);
+  const [nums, setNums] = useState([...nums_init, "", " "]);
   const [accessToken, setAccessToken] = useState("");
   const [isInputPassword, setIsInputPassword] = useState(false);
   const [password, setPassword] = useState("");
   const [password2, setPassword2] = useState("");
   const [message, setMessage] = useState(PASSWORD_INPUT_TEXT);
 
-  useEffect(()=>{
-    let nums_random = Array.from({ length: 10 }, (v, k) => k) // 이 배열을 변경해 입력문자 변경 가능
-    setNums(shuffle([...nums_random,"",""]))
-  },[])
+  useEffect(() => {
+    let nums_random = Array.from({ length: 10 }, (v, k) => k); // 이 배열을 변경해 입력문자 변경 가능
+    setNums(shuffle([...nums_random, "", ""]));
+  }, []);
 
-  useEffect(() => { 
-    if(password.length == PASSWORD_MAX_LENGTH && !isInputPassword) {
-        setIsInputPassword(true);
-        setPassword2(password);
-        setMessage(PASSWORD_REINPUT_Text);
+  useEffect(() => {
+    if (password.length == PASSWORD_MAX_LENGTH && !isInputPassword) {
+      setIsInputPassword(true);
+      setPassword2(password);
+      setMessage(PASSWORD_REINPUT_Text);
+      setPassword("");
+    } else if (password.length == PASSWORD_MAX_LENGTH && isInputPassword) {
+      // 만약 1차 비밀번호와 2차비밀번호가 같지 않다면 다시 1차 비밀번호 입력하도록 한다.
+      if (password != password2) {
+        alert("1차 비밀번호와 2차 비밀번호가 같지 않습니다.");
+        setIsInputPassword(false);
+        setPassword2("");
+        setMessage(PASSWORD_INPUT_TEXT);
         setPassword("");
-    }
-    else if(password.length == PASSWORD_MAX_LENGTH && isInputPassword){
-        
-        // 만약 1차 비밀번호와 2차비밀번호가 같지 않다면 다시 1차 비밀번호 입력하도록 한다.
-        if(password != password2){
-            alert("1차 비밀번호와 2차 비밀번호가 같지 않습니다.");
-            setIsInputPassword(false);
-            setPassword2("");
-            setMessage(PASSWORD_INPUT_TEXT);
-            setPassword("");
-        }
-        else {
-            alert("2차 비밀번호까지 완료!" + password2);
-            saveMyEasyPassword();
-        }
+      } else {
+        alert("2차 비밀번호까지 완료!" + password2);
+        saveMyEasyPassword();
+      }
     }
   }, [password]);
 
   const saveMyEasyPassword = () => {
     const headers: Record<string, string> = {
-        'Content-Type': 'application/json',
-        'Authorization': auth.accessToken
-    }
+      "Content-Type": "application/json",
+      Authorization: auth.accessToken,
+    };
 
-    axios.post(`${BASE_HTTP_URL}/api/user/easyPassword`,
+    axios
+      .post(
+        `${BASE_HTTP_URL}/api/user/easyPassword`,
         {
-            easyPassword: password2
+          easyPassword: password2,
         },
         {
-            headers: headers
-        })
-        .then((res) => {
-            alert("성공 : " + res.data.message);
-            // 성공하면 메인 페이지로 보내기...
-        })
-        .catch((err) => {
-            alert("에러발생 : " + err);
-        })
-  }
+          headers: headers,
+        }
+      )
+      .then((res) => {
+        alert("성공 : " + res.data.message);
+        // 성공하면 메인 페이지로 보내기...
+      })
+      .catch((err) => {
+        alert("에러발생 : " + err);
+      });
+  };
 
   const handlePasswordChange = useCallback(
     (num) => {
       if (password.length === PASSWORD_MAX_LENGTH) {
-        return
+        return;
       }
 
-      setPassword(password + num.toString())
+      setPassword(password + num.toString());
     },
-    [password],
-    )
+    [password]
+  );
 
   const erasePasswordOne = useCallback(
     (e: MouseEvent) => {
-      setPassword(password.slice(0, password.length === 0 ? 0 : password.length - 1))
+      setPassword(
+        password.slice(0, password.length === 0 ? 0 : password.length - 1)
+      );
     },
-    [password],
-  )
+    [password]
+  );
 
   const erasePasswordAll = useCallback((e: MouseEvent) => {
-    setPassword("")
-    let nums_random = Array.from({ length: 10 }, (v, k) => k) // 이 배열을 변경해 입력문자 변경 가능
-    setNums(shuffle([...nums_random,"",""]))
-  }, [])
+    setPassword("");
+    let nums_random = Array.from({ length: 10 }, (v, k) => k); // 이 배열을 변경해 입력문자 변경 가능
+    setNums(shuffle([...nums_random, "", ""]));
+  }, []);
 
   const shuffleNums = useCallback(
     (num: number) => (e: MouseEvent) => {
       // 0 ~ 9 섞어주기
       // let nums_random = Array.from({ length: 10 }, (v, k) => k) // 이 배열을 변경해 입력문자 변경 가능
       // setNums(shuffle([...nums_random,"",""]))
-      handlePasswordChange(num)
+      handlePasswordChange(num);
     },
-    [handlePasswordChange],
-  )
+    [handlePasswordChange]
+  );
 
   const onClickSubmitButton = (e: MouseEvent) => {
     // 비밀번호 제출
     if (password.length === 0) {
-      alert("비밀번호를 입력 후 눌러주세요!")
+      alert("비밀번호를 입력 후 눌러주세요!");
     } else {
-      alert(password + "을 입력하셨습니다.")
+      alert(password + "을 입력하셨습니다.");
     }
-  }
+  };
   return (
     <>
       <Password value={password} />
       <div>{message}</div>
       <div className={styles.inputer}>
-  {[
-    ...nums.map((n,i) => (
-      <button
-        value={n}
-        onClick={shuffleNums(n)}
-        key={i}
-        className={styles.btn}
-      >
-        {n}
-      </button>
-    )),
-  ]}
-</div>
+        {[
+          ...nums.map((n, i) => (
+            <button
+              value={n}
+              onClick={shuffleNums(n)}
+              key={i}
+              className={styles.btn}
+            >
+              {n}
+            </button>
+          )),
+        ]}
+      </div>
 
       <div>
-      <button
-      className='num-button'
-      onClick={erasePasswordAll}
-      key="eraseAll"
-      className={styles.bottom_btm}
-    >
-      X
-    </button>
-  <button
-    className='num-button'
-    onClick={erasePasswordOne}
-    key="eraseOne"
-    className={styles.bottom_btm}
-  >
-    ←
-  </button>
-        <button type='submit' className={styles.bottom_btm} onClick={onClickSubmitButton}>
+        <button
+          className="num-button"
+          onClick={erasePasswordAll}
+          key="eraseAll"
+          className={styles.bottom_btm}
+        >
+          X
+        </button>
+        <button
+          className="num-button"
+          onClick={erasePasswordOne}
+          key="eraseOne"
+          className={styles.bottom_btm}
+        >
+          ←
+        </button>
+        <button
+          type="submit"
+          className={styles.bottom_btm}
+          onClick={onClickSubmitButton}
+        >
           제출
         </button>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SecuritySetting
+export default SecuritySetting;
--- frontend/src/pages/Transfer/AccountDetailComponent.tsx
+++ frontend/src/pages/Transfer/AccountDetailComponent.tsx
@@ -8,8 +8,8 @@ import { setTradeHistorys } from "../../store/slices/tradeHistorySlice";
 import { setBalance } from "../../store/slices/accountSlice";
 import TradeHistory from "../../components/Transfer/TradeHistory";
 
-// const BASE_HTTP_URL = "https://j9E106.p.ssafy.io";
-const BASE_HTTP_URL = "http://localhost:8080";
+const BASE_HTTP_URL = "https://j9E106.p.ssafy.io";
+// const BASE_HTTP_URL = "http://localhost:8080";
 
 function AccountDetailComponent(props) {
   const [tradeHistoryDict, setTradeHistoryDict] = useState<any>(null);
