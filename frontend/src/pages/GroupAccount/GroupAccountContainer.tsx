df89e888e2cfabb81f8063367359b6eacd10022e
unknown <bluemin_@naver.com>
2023-10-05 14:31:08 +0900

    Feat : 프론트 배포용 프론트 배포용

--- frontend/src/pages/GroupAccount/GroupAccountContainer.tsx
+++ frontend/src/pages/GroupAccount/GroupAccountContainer.tsx
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 // import Carousel from "../../common/Carousel";
 import { Carousel } from "react-responsive-carousel";
 import GroupAccountHistory from "../../pages/GroupAccount/GroupAccountHistory";
@@ -16,51 +15,49 @@ const GroupAccountContainer = () => {
       setSelectedBtn("btn1");
     } else if (selectedIndex === 1) {
       setSelectedBtn("btn2");
-    } 
+    }
   };
   return (
     <div>
       <div>
-          <button
-            style={{
-              borderRadius: "100%",
-              width: "10px",
-              height: "10px",
-              backgroundColor: selectedBtn == "btn1" ? "#305dc5" : "#E3E3E3",
-              padding: "0",
-              marginRight: "10px",
-            }}
-            onClick={() => handleButtonClick("btn1")}
-          ></button>
-          <button
-            style={{
-              borderRadius: "100%",
-              width: "10px",
-              height: "10px",
-              backgroundColor: selectedBtn == "btn2" ? "#305dc5" : "#E3E3E3",
-              padding: "0",
-              marginRight: "10px",
-            }}
-            onClick={() => handleButtonClick("btn2")}
-          ></button>
-        </div>
+        <button
+          style={{
+            borderRadius: "100%",
+            width: "10px",
+            height: "10px",
+            backgroundColor: selectedBtn == "btn1" ? "#305dc5" : "#E3E3E3",
+            padding: "0",
+            marginRight: "10px",
+          }}
+          onClick={() => handleButtonClick("btn1")}
+        ></button>
+        <button
+          style={{
+            borderRadius: "100%",
+            width: "10px",
+            height: "10px",
+            backgroundColor: selectedBtn == "btn2" ? "#305dc5" : "#E3E3E3",
+            padding: "0",
+            marginRight: "10px",
+          }}
+          onClick={() => handleButtonClick("btn2")}
+        ></button>
+      </div>
       <Carousel
-      selectedItem={
-        selectedBtn === "btn1" ? 0 :1
-      }
-      showThumbs={false}
-      infiniteLoop={false}
-      showIndicators={false}
-      swipeable={true}
-      showArrows={false}
-      showStatus={false}
-      onChange={handleCarouselChange}
+        selectedItem={selectedBtn === "btn1" ? 0 : 1}
+        showThumbs={false}
+        infiniteLoop={false}
+        showIndicators={false}
+        swipeable={true}
+        showArrows={false}
+        showStatus={false}
+        onChange={handleCarouselChange}
       >
         <div>
-        <GroupAccount />
+          <GroupAccount />
         </div>
         <div>
-        <GroupAccountHistory />
+          <GroupAccountHistory />
         </div>
       </Carousel>
     </div>
